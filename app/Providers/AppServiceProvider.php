a7bba80f22e9f92945ecfd6eae1751fa0fa96648
jeanpierreishimwe <ishimwejeanpierre03@gmail.com>
2023-05-16 09:54:28 +0200

    initial001

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Schema;
 class AppServiceProvider extends ServiceProvider
 {
     /**
-     * Register any application services.
+     * Register any application services.nn
      */
     public function register(): void
     {
