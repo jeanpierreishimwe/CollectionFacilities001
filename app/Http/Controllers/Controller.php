cb78f3bcf1b72bb36cb1ced267a4f0076b731858
jeanpierreishimwe <ishimwejeanpierre03@gmail.com>
2023-05-16 08:36:07 +0200

    intial001

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -8,7 +8,7 @@ use Illuminate\Routing\Controller as BaseController;
 
 /**
 * @OA\Info(
- * title="Authentication",
+ * title="Collectional Facilities BaCK-End Rest Api EndPoints For",
  *      version="1.0.0",
  *     @OA\License(
  *         name="Apache 2.0",
